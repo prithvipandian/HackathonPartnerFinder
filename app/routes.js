8870a22372c79300760e6fd5b3b60bdfc7512d5d
martalson <martinalonso@berkeley.edu>
2014-10-04 21:33:51 -0700

    added mygroup to routes

--- app/routes.js
+++ app/routes.js
@@ -11,4 +11,7 @@ module.exports = function(app) {
     app.get('/register/', function(req, res) {
         res.sendFile(__dirname + "/www/register.html");
     });
+    app.get('/myGroup/', function(req, res) {
+        res.sendFile(__dirname + "/www/myGroup.html");
+    });
 };
