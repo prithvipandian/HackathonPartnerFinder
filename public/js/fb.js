57dfb7115fed47e09b383b6c82d0cdb038bb3877
jaywreddy <jay.w.reddy@gmail.com>
2014-10-04 18:57:40 -0700

    more login work

---
+++ public/js/fb.js
@@ -0,0 +1,38 @@
+function statusChangeCallback(response) {
+    console.log('statusChangeCallback');
+    console.log(response);
+    if (response.status === 'connected') {
+      // Logged into your app and Facebook.
+      //show modal 
+      $('#registerModal').modal('show');
+    } else{
+     console.log("user refused login");
+    }
+  }
+
+  function login(){
+     FB.login(function(response){
+      statusChangeCallback(response);
+     }); 
+  }
+
+  window.fbAsyncInit = function() {
+  FB.init({
+    appId      : '386707621414765',
+    cookie     : true,  // enable cookies to allow the server to access 
+                        // the session
+    xfbml      : true,  // parse social plugins on this page
+    version    : 'v2.1' // use version 2.1
+  });
+
+
+  };
+
+  // Load the SDK asynchronously
+  (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
