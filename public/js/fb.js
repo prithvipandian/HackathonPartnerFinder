cb3aa9ec7ef0a9c4825be280c35bcdaf9b670608
Daniel <d.feldman@berkeley.edu>
2014-10-05 11:43:26 -0700

    Unified UI Tweaked bootstrap css
    adding templating for some callbacks in fb.js?

--- public/js/fb.js
+++ public/js/fb.js
@@ -30,9 +30,35 @@ function joinIdea(groupId,hackathon){
 }
 
 function fbid(callback){
+  if(FB == undefined || FB == null){
+  window.fbAsyncInit = function() {
+  FB.init({
+    appId      : '386707621414765',
+    cookie     : true,  // enable cookies to allow the server to access 
+                        // the session
+    xfbml      : true,  // parse social plugins on this page
+    version    : 'v2.1' // use version 2.1
+  });
+
+
+  };
+
+  // Load the SDK asynchronously
+  (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
+
   FB.getLoginStatus(function(response){
     callback(response.authResponse.userID);
   });
+  }
+
+
+
 }
 
 function registerUser(){
@@ -175,6 +201,7 @@ function statusChangeCallback(response) {
      //})
 
   }
+
 
   window.fbAsyncInit = function() {
   FB.init({
